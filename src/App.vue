dd5ac298efcb567cf296b13b9e1ed96ca68f851f
vovanostm <axb48@yandex.ru>
2019-03-09 16:46:27 +0300

    Навигация перенесена в отдельный компонент

--- src/App.vue
+++ src/App.vue
@@ -1,27 +1,14 @@
 <template>
   <div>
-    <h1>Наше приложение</h1>
-    <nav>
-      <ul>
-        <li>
-          <router-link to="/">Новости</router-link>
-        </li>
-        <li>
-          <router-link to="/chat">Чат</router-link>
-        </li>
-        <li>
-          <router-link to="/store">Магазин</router-link>
-        </li>
-      </ul>
-    </nav>
-    <router-view></router-view>
+    <layout></layout>
   </div>
 </template>
 
 <script>
+import Layout from "@/components/Layout";
 export default {
   name: "App",
-  components: {},
+  components: { Layout },
   data: () => ({
     page: "chat"
   })
---
+++ src/components/Layout.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <h1>Наше приложение</h1>
+    <nav>
+      <ul>
+        <li>
+          <router-link to="/">Новости</router-link>
+        </li>
+        <li>
+          <router-link to="/chat">Чат</router-link>
+        </li>
+        <li>
+          <router-link to="/store">Магазин</router-link>
+        </li>
+      </ul>
+    </nav>
+    <router-view></router-view>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Layout"
+};
+</script>
